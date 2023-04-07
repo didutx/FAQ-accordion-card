3cc00413b899106395c32c63e65659e17a93fc49
didutx <125872952+didutx@users.noreply.github.com>
2023-04-07 14:31:34 +0300

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ let prevItem = null;
                 const prevArrowImage = prevItem.querySelector(".image-arrow img");
 
                 prevInfoDiv.style.display = "block";
-                prevQuestion.style.fontWeight = "400";
+                prevQuestion.style.fontWeight = "700";
                 prevArrowImage.style.transform = "rotate(-180deg)"
             }
             prevItem = item;
